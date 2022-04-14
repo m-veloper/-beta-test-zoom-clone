fa77f557a70cf8b99f2b2b70b8d8799e8c5f7106
m-veloper <m_veloper@naver.com>
2022-04-14 18:29:05 +0900

    메시지에 닉네임 표시하기

--- noom/src/server.js
+++ noom/src/server.js
@@ -20,13 +20,14 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anonymous"
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from Browser"));
   socket.on("message", (msg) => {
     const message = JSON.parse(msg);
     switch (message.type) {
       case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${message.payload}`));
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
       case "nickname":
         socket["nickname"] = message.payload;
     }
